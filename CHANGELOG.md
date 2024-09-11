2fe4f2b8a9ca9f04702d6c8d2b29f6d143d070f3
Zalron <68781399+zaldor30@users.noreply.github.com>
2024-09-11 19:14:56 -0400

    build

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Raid Lead Helper
+## 1.0 - War Within Release
+
+### 1.0.0
+    - First Version
---
+++ Locales/enUS.lua
@@ -0,0 +1,2 @@
+-- Localization file for English/United States
+local L = LibStub("AceLocale-3.0"):NewLocale("GuildRecruiter", "enUS", true)
